f1412ba5034417aab98ddff54cf4e56fcb8037bc
Dmitry Dubina <ord1naryman.dmitry@gmail.com>
2024-03-06 18:06:41 +0300

    add main

--- main.go
+++ main.go
@@ -3,20 +3,38 @@ package main
 import (
 	"net/http"
 
-	"github.com/m8-XD/real_estate_selling_website/api/user_api"
-	"github.com/m8-XD/real_estate_selling_website/db/repository"
+	"github.com/joho/godotenv"
+	"github.com/m8-XD/real_estate_selling_website/api"
 	"github.com/m8-XD/real_estate_selling_website/logging"
-	"gorm.io/gorm"
 )
 
-type Server struct {
-	DB *gorm.DB
-}
-
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("cannot load .env file")
+	}
+
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/testdb", user_api.CreateUser)
+	// mux.HandleFunc("/") //show all posts
+	mux.HandleFunc("/register", func(rw http.ResponseWriter, r *http.Request) {
+		http.ServeFile(rw, r, "./static/auth/register.html")
+	})
+	mux.HandleFunc("/error", func(rw http.ResponseWriter, r *http.Request) {
+		http.ServeFile(rw, r, "./static/error/"+r.URL.Query().Get("code")+".html")
+	})
+	mux.HandleFunc("/post/create", func(rw http.ResponseWriter, r *http.Request) {
+		http.ServeFile(rw, r, "./static/post/create.html")
+	})
+
+	mux.HandleFunc("POST /api/v1/user/email", api.ValidateEmail)
+	// mux.HandleFunc("POST /api/v1/user/delete/{id}")
+	// mux.HandleFunc("POST /api/v1/user/edit/{id}")
+	mux.HandleFunc("POST /api/v1/user/create", api.CreateUser)
+	mux.HandleFunc("POST /api/v1/post/create", api.CreatePost)
+	// mux.HandleFunc("POST /api/v1/post/delete/{id}")
+	// mux.HandleFunc("POST /api/v1/post/edit/{id}")
+	// mux.HandleFunc("/post/{id}")
 
 	logging.Ok("server has started")
 	_ = http.ListenAndServe(":8080", mux)
